1fa8d4e11ae4a21b3a1168ac334435df7de1167b
Mark Paluch <mpaluch@paluch.biz>
2013-03-27 15:04:35 +0100

    Bugfixes

--- src/main/java/de/paluch/configserver/rest/NotFoundExceptionMapper.java
+++ src/main/java/de/paluch/configserver/rest/NotFoundExceptionMapper.java
@@ -5,11 +5,13 @@ import de.paluch.configserver.model.NotFoundException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
 /**
  * @author <a href="mailto:mpaluch@paluch.biz">Mark Paluch</a>
  * @since 08.01.13 20:53
  */
+@Provider
 public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
     @Override
     public Response toResponse(NotFoundException e) {
--- src/main/java/de/paluch/configserver/rest/UnconfiguredHostExceptionMapper.java
+++ src/main/java/de/paluch/configserver/rest/UnconfiguredHostExceptionMapper.java
@@ -5,17 +5,17 @@ import de.paluch.configserver.model.UnconfiguredHostException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
 /**
  * @author <a href="mailto:mpaluch@paluch.biz">Mark Paluch</a>
  * @since 08.01.13 20:53
  */
+@Provider
 public class UnconfiguredHostExceptionMapper implements ExceptionMapper<UnconfiguredHostException> {
     @Override
     public Response toResponse(UnconfiguredHostException e) {
-        return Response.status(Response.Status.NOT_FOUND).
-                type(MediaType.TEXT_PLAIN_TYPE).
-                               entity("Cannot resolve " + e.getMessage() +
-                                              " to an environment").build();
+        return Response.status(Response.Status.NOT_FOUND).type(MediaType.TEXT_PLAIN_TYPE).
+                entity("Cannot resolve " + e.getMessage() + " to an environment").build();
     }
 }
